2e4f82ae039d9910038ed858f36eb1f23848824f
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-06-15 01:32:56 +0530

    Added health cheack api call .. to get the server running beforehand

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,25 @@ import "./App.css";
 import Home from "./components/Home/Home";
 import FloatingIcons from "./components/FloatingIcons/FloatingIcons";
 import ProjectShowcase from "./components/Projects/Projectshowcase";
+import { chatServer } from "./components/chat/chatServer";
 
 function App() {
+  useEffect(() => {
+    const checkServerHealth = async () => {
+      try {
+        const res = await fetch(`${chatServer}/health`, {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        });
+        console.log("Server Health Check Response:", await res.text());
+      } catch (error) {
+        console.error("Health check failed:", error);
+      }
+    };
+
+    checkServerHealth();
+  }, []);
+
   return (
     <div className="min-h-screen overflow-x-hidden ">
       <FloatingIcons />
